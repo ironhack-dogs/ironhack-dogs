08477c0a5a16be3d40a4bc51a52dea17e345a50d
A-Tr <alvarotrancon@gmail.com>
2018-04-23 14:22:04 +0200

    CRUD Edit Center Added

--- routes/centers.js
+++ routes/centers.js
@@ -2,15 +2,23 @@ const express = require("express");
 const router = express.Router();
 const Center = require("../models/Center");
 
+// List all centers
 router.get("/centers", (req, res, next) => {
   res.render("centers/index", { user: req.user });
 });
 
+// Show one center
 router.get("/centers/:id", (req, res, next) => {
-  res.render("centers/profile", { user: req.user });
+  Center.findById(req.params.id).then(center =>
+    res.render("centers/profile", { user: req.user, center })
+  );
 });
 
-// router.get("/centers/:id/edit", "middleware", (req, res, next) => {
-//   res.render("centers/profile", { user: req.user });
-// });
+// CRUD Edit Center
+router.get("/centers/:id/edit", (req, res, next) => {
+  Center.findById(req.params.id).then(center =>
+    res.render("centers/edit", { user: req.user, center })
+  );
+});
+
 module.exports = router;
