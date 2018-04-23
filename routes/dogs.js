dd008ec786118ff29370271193f69bff14401947
aberouch <a.aberouch@gmail.com>
2018-04-23 18:06:19 +0200

    Añadido Edit perros

--- routes/dogs.js
+++ routes/dogs.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const User = require("../models/Dog");
 const Dog = require("../models/Dog");
 const Center = require("../models/Center");
+const moment = require('moment');
+
+
+// Create new dog
 
 router.get("/new", (req, res, next) => {
   Center.findOne({'admin_id':req.user.id})
   .then(centerData => {
-    console.log(centerData);
     res.render("dogs/new", {centerData});
   });
 });
@@ -22,6 +25,27 @@ router.post("/new", (req, res, next) => {
   .catch(e => next(e))
 });
 
+
+// Edit dog
+
+router.get("/edit/:id", (req, res, next) => {
+  Dog.findById(req.params.id)
+  .then(dogData => {  
+    dogData.formatedDate =  moment(dogData.birthday).format('YYYY-MM-DD');
+    res.render("dogs/edit", {dogData});
+  });
+});
+
+router.post("/edit/:id", (req, res, next) => {
+    const { name, breed, gender, birthday, size, color, hair, description } = req.body;
+    const updates = { name, breed, gender, birthday, size, color, hair, description};
+    Dog.findByIdAndUpdate(req.params.id, updates).then(() => {
+      res.redirect(`/dogs/${req.params.id}`);
+    });
+  });
+
+
+// Dog index and Dog profile
 
 router.get("/", (req, res, next) => {
   Dog.find()
@@ -38,10 +62,6 @@ router.get("/:id", (req, res, next) => {
     res.render("dogs/profile", {dogData});
   });
 });
-
-
-
-
 
  
 module.exports = router;
