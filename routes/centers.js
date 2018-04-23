c4e1a00f565ae15f46db4eaf23e9555a7bb74401
aberouch <a.aberouch@gmail.com>
2018-04-23 12:20:15 +0200

    Changed routes and add User, Centers routes

---
+++ routes/centers.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const Center = require("../models/Center");
+
+router.get("/centers", (req, res, next) => {
+  res.render("centers/index", { user: req.user });
+});
+
+router.get("/centers/:id", (req, res, next) => {
+  res.render("centers/profile", { user: req.user });
+});
+
+module.exports = router;
