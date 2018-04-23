40c5bbed6f001eaae3416d58a0b55ceb82b732d3
A-Tr <alvarotrancon@gmail.com>
2018-04-23 15:45:07 +0200

    Added conditional navbar

--- routes/centers.js
+++ routes/centers.js
@@ -9,24 +9,31 @@ router.get("/centers", (req, res, next) => {
 
 // Show one center
 router.get("/centers/:id", (req, res, next) => {
-  Center.findById(req.params.id).then(center =>
-    res.render("centers/profile", { user: req.user, center })
-  );
+  Center.findById(req.params.id)
+    .then(center => res.render("centers/profile", { user: req.user, center }))
+    .catch(e => next(e));
 });
 
 // CRUD Edit Center
 router.get("/centers/:id/edit", (req, res, next) => {
-  Center.findById(req.params.id).then(center =>
-    res.render("centers/edit", { user: req.user, center })
-  );
+  Center.findById(req.params.id)
+    .then(center => res.render("centers/edit", { user: req.user, center }))
+    .catch(e => next(e));
 });
 
 router.post("/centers/:id/edit", (req, res, next) => {
   const { name, phone, email, website_url, description } = req.body;
   const update = { name, phone, email, website_url, description };
   Center.findByIdAndUpdate(req.params.id, update)
-  .then(() => res.redirect(`/centers/${req.params.id}`))
+    .then(() => res.redirect(`/centers/${req.params.id}`))
+    .catch(e => next(e));
+});
 
-})
+//CRUD Delete Center
+router.get("/centers/:id/delete", (req, res, next) => {
+  Center.findByIdAndRemove(req.params.id)
+    .then(() => res.redirect("/my-profile"))
+    .catch(e => next(e));
+});
 
 module.exports = router;
