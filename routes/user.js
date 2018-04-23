f77b589784298f36dd3e248ba157664e085e2813
A-Tr <alvarotrancon@gmail.com>
2018-04-23 14:05:42 +0200

    Able to create a center, content of profile page changes

--- routes/user.js
+++ routes/user.js
@@ -3,17 +3,23 @@ const router = express.Router();
 const Center = require("../models/Center");
 
 
+// SEE PROFILE
 router.get("/", (req, res, next) => {
-  res.render("user/profile", { user: req.user });
+  Center.findOne({"admin_id": req.user.id}).then(center => {
+    console.log(center)
+    res.render("user/profile", { user: req.user, center });
+  });
 });
 
+
+// CRUD CREATE CENTER
 router.get("/create-center", (req, res, next) => {
   res.render("user/create-center", { user: req.user });
 });
 
 router.post("/create-center", (req, res, next) => {
   const { name, phone, email, website_url, description } = req.body;
-  console.log(req.user)
+  console.log(req.user);
   const newCenter = new Center({
     name,
     email,
@@ -21,9 +27,7 @@ router.post("/create-center", (req, res, next) => {
     description,
     admin_id: req.user.id
   });
-  newCenter.save().then(() => res.redirect("/my-profile"))
-
+  newCenter.save().then(() => res.redirect("/my-profile"));
 });
-
 
 module.exports = router;
