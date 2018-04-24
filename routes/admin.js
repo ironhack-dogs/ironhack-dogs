defc2d55b395187e9a3656b694a1ab5c758644b6
A-Tr <alvarotrancon@gmail.com>
2018-04-24 18:19:50 +0200

    añadida la ruta de pasar usuario a admin

--- routes/admin.js
+++ routes/admin.js
@@ -2,16 +2,32 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const Request = require("../models/Request");
-const isSuperadmin = require("../middleware/isSuperadmin");
+const isSuperAdmin = require("../middleware/isSuperadmin");
 
-router.get("/", isSuperadmin(), (req, res, next) => {
+router.get("/", isSuperAdmin(), (req, res, next) => {
   Request.find()
-  .populate("user")
+    .populate("user")
     .then(requests => {
-      res.render("admin/index", {requests});
+      res.render("admin/index", {user:req.user, requests });
     })
     .catch(e => next(e));
 });
 
+
+// Aceptar solicitud para convertirse en Admin
+router.get("/:id/accept", isSuperAdmin(), (req, res, next) => {
+  Request.findByIdAndUpdate(req.params.id, { status: "Accepted" })
+    .then(request => {
+      User.findByIdAndUpdate(request.user, { role: "Admin", isAdmin: "true" })
+        .then(res.redirect("/admin"))
+        .catch(e => next(e));
+    })
+    .catch(e => next(e));
+});
+
+// Rechazar solicitud para convertirse en Admin
+router.get("/:id/deny", isSuperAdmin(), (req, res, next) => {
+
+});
 
 module.exports = router;
