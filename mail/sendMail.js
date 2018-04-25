d531390a9884c634a49eed38731555b2a3d62211
A-Tr <alvarotrancon@gmail.com>
2018-04-25 13:34:54 +0200

    maquetado email de bienvenida

--- mail/sendMail.js
+++ mail/sendMail.js
@@ -5,16 +5,21 @@ const sendAwesomeMail = (to, from = "admin@madrid.com") => {
   transporter.sendMail({
     to,
     from,
-    subject: "hola que tal",
+    subject: "Bienvenido a Madrid Adopta",
     html: `<body style="font-family: 'Open Sans', sans-serif;font-size: 16px;">
-    <header height="600" style="background-image: url(dog_welcome.jpg);background-size: cover;height: 600px;width: 100%;">
-      <img src="http://res.cloudinary.com/dxsy5lr4t/image/upload/v1524590336/Logo.png" alt="logo">
+    <header style="background-image: url(http://res.cloudinary.com/abdaio/image/upload/v1524566491/mascotas-header.jpg);background-size: cover;height: 400px;width: 100%;">
+    <img src="http://res.cloudinary.com/dxsy5lr4t/image/upload/v1524590336/logo-solo.png" alt="logo" id="imgone" style="display: block;padding: 50px;float: left;">
+    <img src="http://res.cloudinary.com/dxsy5lr4t/image/upload/v1524590336/logo-text.png" alt="logo" id="imgtwo" style="display: block;padding: 50px;float: right;">
     </header>
-    <div>
-      <p>Tu solicitud ha sido procesada y has sido dado de alta, puedes crear tu centro haciendo click en el siguiente enlace:
-        <a href=""></a>
-      </p>
+    <div id="content" style="max-width: 768px;height: 300px;margin: 0 auto;">
+    <h1 style="text-align: center;font-weight: normal;color: #924992;">Bienvenido a Madrid Adopta</h1>
+    <p>Tu solicitud ha sido procesada y has sido dado de alta, ya puedes crear tu centro haciendo click en
+      <a href="http://localhost:3000/my-profile" style="font-weight: normal;color: #924992;">este enlace</a>
+    </p>
     </div>
+    <footer style="background-color: black;color: white;text-align: center;height: 100px;line-height: 100px;">
+    <p>Copyright</p>
+    </footer>
     </body>`
   });
 };
