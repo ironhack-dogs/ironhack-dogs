437c289046fc5f125b20e7c8340dafeae91dd7eb
A-Tr <alvarotrancon@gmail.com>
2018-04-25 16:51:09 +0200

    Added denied permission email

--- routes/admin.js
+++ routes/admin.js
@@ -3,43 +3,41 @@ const router = express.Router();
 const User = require("../models/User");
 const Request = require("../models/Request");
 const isSuperAdmin = require("../middleware/isSuperadmin");
-const sendWelcomeMail = require("../mail/sendWelcomeMail")
+const sendWelcomeMail = require("../mail/sendWelcomeMail");
+const sendDenyMail = require("../mail/sendDenyMail");
 
 router.get("/", isSuperAdmin(), (req, res, next) => {
   Request.find()
     .populate("user")
     .then(requests => {
-      res.render("admin/index", {user:req.user, requests });
+      res.render("admin/index", { user: req.user, requests });
     })
     .catch(e => next(e));
 });
-
 
 // Aceptar solicitud para convertirse en Admin
 router.get("/:id/accept", isSuperAdmin(), (req, res, next) => {
   Request.findByIdAndUpdate(req.params.id, { status: "Accepted" })
-  .then(request => {
-    User.findByIdAndUpdate(request.user, { role: "Admin", isAdmin: "true" })
-    .then(user => {
-      console.log(user.email)
-      sendWelcomeMail(user.email);
-      res.redirect("/admin")   
+    .then(request => {
+      User.findByIdAndUpdate(request.user, { role: "Admin", isAdmin: "true" })
+        .then(user => {
+          console.log(user.email);
+          sendWelcomeMail(user.email);
+          res.redirect("/admin");
+        })
+        .catch(e => next(e));
     })
     .catch(e => next(e));
-  })
-  .catch(e => next(e));
 });
 
 // Rechazar solicitud para convertirse en Admin
 router.get("/:id/deny", isSuperAdmin(), (req, res, next) => {
-  Request.findById(req.params.id)
-  .then(request => {
-    User.findById(request.user)
-    .then(user => {
-      sendWelcomeMail(user.email);
-      res.redirect("/admin")
-    })
-  })
+  Request.findById(req.params.id).then(request => {
+    User.findById(request.user).then(user => {
+      sendDenyMail(user.email);
+      res.redirect("/admin");
+    });
+  });
 });
 
 module.exports = router;
