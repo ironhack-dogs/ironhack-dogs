947945cdf4ef183f7deee0a4069884efee3a8746
aberouch <a.aberouch@gmail.com>
2018-04-26 11:04:32 +0200

    Changed Dog profiles (hiding actions no users that are not logged in) and changed data seeds

--- bin/seeds/dog_seed.js
+++ bin/seeds/dog_seed.js
@@ -12,7 +12,8 @@ const momentRandom = require('moment-random');
 const promiseArray = [];
 const genders = ["Macho", "Hembra"];
 const sizes = ["Pequeño", "Mediano", "Grande"];
-const breeds = ["Pastor Aleman", "Labrador", "Corgi", "Bulldog", "Mestizo"];
+const colors = ["Negro", "Blanco", "Marrón","Canela","Gris"];
+const breeds = ["Pastor Alemán", "Labrador", "Corgi", "Bulldog", "Mestizo","Galgo"];
 const hairs = ["Corto", "Medio", "Largo"]
 
 const prueba = (dog) => {
@@ -20,12 +21,14 @@ const prueba = (dog) => {
     dogAPI.getOneRegister().then(data => {
       dog.picture_url = data.message;
       dog.name = dogNames.allRandom();
-      dog.birthday = moment(momentRandom("2015-12-25", "2000-12-25")).format('YYYY MM DD')
-      dog.center = "5addb2588cc03f34689efd87"
+      dog.birthday = moment(momentRandom("2018-02-25", "2014-12-25")).format('YYYY MM DD')
+      dog.center = "5addb2588cc03f34689efd87";
       dog.gender = genders[Math.round(Math.random())];
       dog.size = sizes[Math.floor(Math.random() * 3)];
-      dog.breed = breeds[Math.floor(Math.random() * 5)];
-      dog.hair = hairs[Math.floor(Math.random() * 3)]
+      dog.color = colors[Math.floor(Math.random() * 5)];
+      dog.breed = breeds[Math.floor(Math.random() * 6)];
+      dog.hair = hairs[Math.floor(Math.random() * 3)];
+      dog.description = "Morbi eu elit ultricies, tristique purus nec, euismod est. Vivamus aliquam odio eu commodo porta. Duis ultrices magna pellentesque ornare dictum. Nullam arcu neque, mattis id aliquet ac, cursus et lacus. Nulla ut iaculis massa. Aliquam imperdiet ex sem, tristique vulputate ante pretium id. Suspendisse molestie luctus interdum. Quisque ornare arcu dolor, sit amet cursus nisi vestibulum id. Sed cursus a eros viverra iaculis. Aenean in elit ut odio sollicitudin tincidunt. Pellentesque quis dapibus libero. Sed cursus dolor ex, id congue diam porttitor id. Integer sollicitudin eget justo eget viverra. "
       resolve(dog);
     });
   });
